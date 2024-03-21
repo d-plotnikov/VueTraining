995eab8eba99da93d63f0db3154eeaf98af32b27
d-plotnikov <d.plotnikov@labmedia.su>
2024-03-21 17:25:38 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
   <div>
-    <Menu />
-    <router-view/>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-import '@/style/style.scss';
-import Menu from './components/Menu'
-
-
-export default {
-  components: { Menu },
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-</script>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
